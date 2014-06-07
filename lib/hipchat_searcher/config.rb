cbe1341866835bf697118a8eed4a31e02958165f
mgi166 <skskoari@gmail.com>
2014-06-08 00:15:10 +0900

    * if config file doesn't exist, exit 1

--- lib/hipchat_searcher/config.rb
+++ lib/hipchat_searcher/config.rb
@@ -1,4 +1,32 @@
 class HipchatSearcher
   class Config
+    def initialize
+      print_not_exist! if File.exist?(config_path)
+      @config = File.read(config_path)
+    end
+
+    def valid?
+    end
+
+    def print_not_exist
+      puts <<-EOS
+Config file is not exsited.
+
+* To create config file, run this command
+`echo {auth_token} > .hps`
+
+* To get new auth_token, visit and sign in.
+https://www.hipchat.com/
+      EOS
+    end
+
+    def print_not_exist!
+      print_not_exist
+      exit 1
+    end
+
+    def config_path
+      File.expand_path(File.join('.', '.hps'))
+    end
   end
 end
