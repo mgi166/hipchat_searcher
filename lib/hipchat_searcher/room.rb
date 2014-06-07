78bdcd8bd892504facebd4894bf4383fa75e7ca6
mgi166 <skskoari@gmail.com>
2014-06-08 08:58:06 +0900

    * Get all room list that is non-archived

---
+++ lib/hipchat_searcher/room.rb
@@ -0,0 +1,35 @@
+require 'hipchat'
+require 'httparty'
+
+class HipchatSearcher
+  class Room
+    include HTTParty
+
+    def initialize(token, options={})
+      @token = token
+      default_options = { api_version: 'v2', server_url: 'https://api.hipchat.com' }
+      @option = default_options.merge(options)
+      @api = ::HipChat::ApiVersion::Room.new(@option)
+      self.class.base_uri(@api.base_uri)
+    end
+
+    def get_all_room
+      response = self.class.get(
+        '',
+        query: {auth_token: @token},
+        header: @api.headers
+      )
+
+      case response.code
+      when 200
+        response.parsed_response
+      when 404
+        raise ::HipChat::UnknownRoom, 'Unknown room'
+      when 401
+        raise ::HipChat::Unauthorized, 'Access denied to room'
+      else
+        raise ::HipChat::UnknownResponseCode, "Unexpected #{response.code} for room"
+      end
+    end
+  end
+end
