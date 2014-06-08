b3268dc15fe38f6d978d2b815e33f87dd2612661
mgi166 <skskoari@gmail.com>
2014-06-08 09:39:38 +0900

    * Add "get_message" method

--- lib/hipchat_searcher/message.rb
+++ lib/hipchat_searcher/message.rb
@@ -1,4 +1,16 @@
+require 'hipchat'
+
 class HipchatSearcher
   class Message
+    def initialize(token, room, options={})
+      @token   = token
+      @room    = room
+      @options = options
+      @client  = ::HipChat::Client.new(@token, api_version: 'v2')
+    end
+
+    def get_message
+      @client[@room].history(@options)
+    end
   end
 end
