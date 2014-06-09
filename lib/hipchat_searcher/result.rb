6a7bdc8765924a4745de37804a957e5d55ff89df
mgi166 <skskoari@gmail.com>
2014-06-09 21:40:06 +0900

    * Create method "room_list" that parse room name from response json

--- lib/hipchat_searcher/result.rb
+++ lib/hipchat_searcher/result.rb
@@ -1,4 +1,10 @@
 class HipchatSearcher
   class Result
+    def initialize(response)
+      @response = response
+    end
+
+    def room_list
+    end
   end
 end
---
+++ spec/lib/hipchat_searcher/result_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe HipchatSearcher::Result do
+  def result(response)
+    described_class.new(response)
+  end
+
+  describe '#room_list' do
+  end
+end
