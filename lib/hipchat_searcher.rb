c9446131736632e90e52c748723dddcf7fac47ff
mgi166 <skskoari@gmail.com>
2014-06-15 00:43:49 +0900

    * Add a new file

--- lib/hipchat_searcher.rb
+++ lib/hipchat_searcher.rb
@@ -1,8 +1,6 @@
-class HipchatSearcher
-end
-
 require 'hipchat_searcher/config'
 require 'hipchat_searcher/message'
+require 'hipchat_searcher/options'
 require 'hipchat_searcher/result'
 require 'hipchat_searcher/room'
 require 'hipchat_searcher/searcher'
