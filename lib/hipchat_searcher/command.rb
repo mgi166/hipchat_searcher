dc4bfc8480eb4efea0f55ed2be5b444bcbdb22bb
mgi166 <skskoari@gmail.com>
2014-06-14 12:19:21 +0900

    * Add "Command#run" method but it is experimental

--- lib/hipchat_searcher/command.rb
+++ lib/hipchat_searcher/command.rb
@@ -1,6 +1,23 @@
 class HipchatSearcher
   class Command
     def initialize
+      @config  = Config.new
+    end
+
+    def run
+      rooms = if @config.has_room?
+                @config.room
+              else
+                room = Room.new(@config.token, @config.option)
+                room.names
+              end
+
+      message  = Message.new(@config.token, @config.option)
+
+      rooms.inject([]) do |result, room|
+        hist = message.history(room)
+        Searcher.search(pattern, hist)
+      end
     end
   end
 end
