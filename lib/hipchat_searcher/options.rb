23b1994b20bfa33cc713f4af9d139fc8aff139eb
mgi166 <skskoari@gmail.com>
2014-06-15 00:59:08 +0900

    * Add new method "room?" and spec

--- lib/hipchat_searcher/options.rb
+++ lib/hipchat_searcher/options.rb
@@ -3,6 +3,7 @@ require 'hashie/mash'
 class HipchatSearcher
   class Options < Hashie::Mash
     def room?
+      !!self['r'] || !!self['room']
     end
   end
 end
--- spec/lib/hipchat_searcher/options_spec.rb
+++ spec/lib/hipchat_searcher/options_spec.rb
@@ -2,13 +2,25 @@ require 'spec_helper'
 
 describe HipchatSearcher::Options do
   describe '#room?' do
-    context 'description' do
-      subject { described_class.new(hash) }
+    context 'given hash has a "room" key as argument' do
+      subject { described_class.new(hash).room? }
       let(:hash) { {'room' => 'room-name' } }
 
-      it 'description' do
-        p subject
-      end
+      it { should be_truthy }
+    end
+
+    context 'given hash has a "r" key as argument' do
+      subject { described_class.new(hash).room? }
+      let(:hash) { {'r' => 'room-name' } }
+
+      it { should be_truthy }
+    end
+
+    context 'given hash have any other keys as argument' do
+      subject { described_class.new(hash).room? }
+      let(:hash) { {'other' => 'xxxx' } }
+
+      it { should be_falsey }
     end
   end
 end
