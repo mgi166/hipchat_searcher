29ee4ef8b3856a77d6e6792b22643fb5949e1328
mgi166 <skskoari@gmail.com>
2014-06-17 23:31:12 +0900

    * Rename #display → #puts_search_result * Add new method #puts_room

--- lib/hipchat_searcher/searcher.rb
+++ lib/hipchat_searcher/searcher.rb
@@ -3,8 +3,9 @@ require 'colorize'
 class HipchatSearcher
   class Searcher
     def initialize(result, options={})
-      @result  = result
-      @options = options
+      @result     = result
+      @options    = options
+      @print_room = false
     end
 
     def self.search(pattern, result, options)
@@ -16,17 +17,32 @@ class HipchatSearcher
 
       @result.items.each do |item|
         if pattern =~ item.message
-          puts display(pattern, item.message)
+          @print_room ? nil : puts_room
+          puts_search_result(pattern, item)
+          puts
         end
       end
 
       nil
     end
 
-    def display(pattern, string)
-      string.gsub(pattern) do |matched|
+    def room
+      @result.room
+    end
+
+    def puts_room
+      @print_room = true
+      puts room
+    end
+
+    def puts_search_result(pattern, item)
+      msg = item.message.gsub(pattern) do |matched|
         matched.underline
       end
+
+      date = "  Date: #{item.date}"
+      msg  = "  @#{item.from.mention_name}" + ': ' + msg
+      puts "%s\n%s" % [date, msg]
     end
   end
 end
--- spec/lib/hipchat_searcher/searcher_spec.rb
+++ spec/lib/hipchat_searcher/searcher_spec.rb
@@ -10,13 +10,50 @@ describe HipchatSearcher::Searcher do
       subject { searcher(result).search(pattern) }
 
       let(:pattern)  { 'yare' }
-      let(:result)   { HipchatSearcher::Result.new(response) }
       let(:response) { File.read(File.join('spec', 'data', 'item_list.json')) }
+      let(:result) do
+        r = HipchatSearcher::Result.new(response)
+        r.room = "Joestars"
+        r
+      end
+      let(:search_result) do
+        "Joestars" + "\n" + \
+        "  Date: 2014-05-30T01:38:16.741565+00:00" + "\n" + \
+        "  @jotaro: \e[4;39;49myare\e[0m\e[4;39;49myare\e[0m daze" + "\n" + \
+        "\n"
+      end
 
-      it 'should print matched string' do
+      it 'should print roomname & search result' do
         expect do
           subject
-        end.to output("\e[4;39;49myare\e[0m\e[4;39;49myare\e[0m daze\n").to_stdout
+        end.to output(search_result).to_stdout
+      end
+    end
+
+    context 'when matches pattern in many messages' do
+      subject { searcher(result).search(pattern) }
+
+      let(:pattern)  { 'ze' }
+      let(:response) { File.read(File.join('spec', 'data', 'item_list.json')) }
+      let(:result) do
+        r = HipchatSearcher::Result.new(response)
+        r.room = "Joestars"
+        r
+      end
+      let(:search_result) do
+        "Joestars" + "\n" + \
+        "  Date: 2014-05-30T01:38:16.741565+00:00" + "\n" + \
+        "  @jotaro: yareyare da\e[4;39;49mze\e[0m" + "\n" \
+        "\n" + \
+        "  Date: 2014-06-09T11:29:10.209014+00:00" + "\n" + \
+        "  @polnareff: a... arinomama ima okotta koto wo hanasu \e[4;39;49mze\e[0m" + "\n" + \
+        "\n"
+      end
+
+      it 'should print roomname & search result' do
+        expect do
+          subject
+        end.to output(search_result).to_stdout
       end
     end
 
@@ -49,17 +86,27 @@ describe HipchatSearcher::Searcher do
         end.to raise_error
       end
     end
-p  end
+  end
 
-  describe '#display' do
-    subject { searcher(result).display(pattern, string) }
+  describe '#puts_search_result' do
+    subject { searcher(double(:result)).puts_search_result(pattern, item) }
 
-    let(:result) { double(:result) }
-    let(:pattern) { 'fuga' }
-    let(:string) { 'hogefugahoge' }
+    let(:pattern) { Regexp.new('yare') }
+    let(:item) do
+      src  = File.read(File.join('spec', 'data', 'item_list.json'))
+      hash = JSON.parse(src)
+      ::Hashie::Mash.new(hash).items.first
+    end
 
-    it 'should add excape escape sequence on matched string' do
-      should == "hoge\e[4;39;49mfuga\e[0mhoge"
+    let(:search_result) do
+      "  Date: 2014-05-30T01:38:16.741565+00:00" + "\n" + \
+      "  @jotaro: \e[4;39;49myare\e[0m\e[4;39;49myare\e[0m daze" + "\n"
+    end
+
+    it 'should print string of search result' do
+      expect do
+        subject
+      end.to output(search_result).to_stdout
     end
   end
 end
