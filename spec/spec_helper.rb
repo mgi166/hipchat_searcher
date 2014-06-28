471c19fb272bd7c81a9d291f0022d35847d8930e
mgi166 <skskoari@gmail.com>
2014-06-29 08:45:16 +0900

    * Enable both syntaxes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,7 @@ RSpec.configure do |config|
   config.order = :random
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
