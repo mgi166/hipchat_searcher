cadf6c35ffaa0cf0d4111cc8d043d86dc08f656d
mgi166 <skskoari@gmail.com>
2014-06-30 09:47:56 +0900

    * Version = 0.0.3

--- spec/hipchat_searcher_spec.rb
+++ spec/hipchat_searcher_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe HipchatSearcher do
   it 'has a version number' do
-    HipchatSearcher::VERSION.should == '0.0.2'
+    HipchatSearcher::VERSION.should == '0.0.3'
   end
 end
