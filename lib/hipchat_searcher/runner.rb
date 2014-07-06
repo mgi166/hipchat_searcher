208ddfb560909eeb4029d4171c6a9ee16f17fcfa
mgi166 <skskoari@gmail.com>
2014-07-06 16:21:56 +0900

    * Fix bug in Runner#new ("@config.token" to "config.token")

--- lib/hipchat_searcher/runner.rb
+++ lib/hipchat_searcher/runner.rb
@@ -3,8 +3,12 @@ module HipchatSearcher
     def initialize(pattern, room, config, options)
       @pattern = pattern
       @room    = room
-      @message = Message.new(@config.token, @options.message_options)
+      @message = Message.new(config.token, options.message_options)
       @options = options
+    end
+
+    def self.run(pattern, room, config, options)
+      new(pattern, room, config, options).run
     end
 
     def run
