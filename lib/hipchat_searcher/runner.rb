a5f71c3b53b8dac2fd491e19390e79ad0f491684
mgi166 <skskoari@gmail.com>
2014-07-06 14:26:29 +0900

    * Create "Runner" class

---
+++ lib/hipchat_searcher/runner.rb
@@ -0,0 +1,13 @@
+module HipchatSearcher
+  class Runner
+    def initialize(pattern, room, config, options)
+      @pattern = pattern
+      @room    = room
+      @config  = config
+      @options = options
+    end
+
+    def run
+    end
+  end
+end
