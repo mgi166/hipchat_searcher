b8cb4f645dd79cb3718362d47a2c9b0d9f6d458b
mgi166 <skskoari@gmail.com>
2014-07-07 23:26:52 +0900

    * Receive arguments of option

--- lib/hipchat_searcher/message.rb
+++ lib/hipchat_searcher/message.rb
@@ -8,15 +8,16 @@ module HipchatSearcher
       @client  = ::HipChat::Client.new(@token, api_version: 'v2')
     end
 
-    def get_history(id)
-      @client[id].history(@options)
+    def get_history(id, options)
+      option = @options.merge(options)
+      @client[id].history(option)
     end
 
-    def history(room)
+    def history(room, options={})
       id        = room.id   rescue room
       room_name = room.name rescue room
 
-      h = get_history(id)
+      h = get_history(id, options)
       Result.new(h).tap {|r| r.room = room_name }
     end
   end
