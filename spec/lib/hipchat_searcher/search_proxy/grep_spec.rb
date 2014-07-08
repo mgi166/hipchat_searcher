3e46e07ae9a592069f84ce247f2b5b9a8d6bfc3b
mgi166 <skskoari@gmail.com>
2014-07-08 09:49:04 +0900

    * Fix range because place above recently comment

--- spec/lib/hipchat_searcher/search_proxy/grep_spec.rb
+++ spec/lib/hipchat_searcher/search_proxy/grep_spec.rb
@@ -29,42 +29,42 @@ describe HipchatSearcher::SearchProxy::Grep do
         let(:result)  { double(:result, items: %w|1 2 3 4 5 6|) }
 
         it 'should return the array of range to include index you specify' do
-          should == %w|1 2 3 4|
+          should == %w|4 5 6|
         end
       end
 
       context 'when specify the range is exluded in the array' do
-        subject { searcher(pattern, result, before_context: '3').around_items(1) }
+        subject { searcher(pattern, result, before_context: '3').around_items(4) }
 
         let(:pattern) { 'fuga' }
         let(:result)  { double(:result, items: %w|1 2 3 4 5 6|) }
 
         it 'should return the array includes range until index from first' do
-          should == %w|1 2|
+          should == %w|5 6|
         end
       end
     end
 
     describe 'you specify :after_context option' do
       context 'when specify the range is included in the array' do
-        subject { searcher(pattern, result, after_context: '3').around_items(1) }
+        subject { searcher(pattern, result, after_context: '3').around_items(3) }
 
         let(:pattern) { 'fuga' }
         let(:result)  { double(:result, items: %w|1 2 3 4 5 6|) }
 
         it 'should return the array of range to include index you specify' do
-          should == %w|2 3 4 5|
+          should == %w|2 3 4|
         end
       end
 
       context 'when specify the range is exluded in the array' do
-        subject { searcher(pattern, result, after_context: '3').around_items(4) }
+        subject { searcher(pattern, result, after_context: '3').around_items(1) }
 
         let(:pattern) { 'fuga' }
         let(:result)  { double(:result, items: %w|1 2 3 4 5 6|) }
 
         it 'should return the array includes range until the end from index' do
-          should == %w|5 6|
+          should == %w|1 2|
         end
       end
     end
