78bb5fcd4fa3cdee437d01c7558ac6cd4429a2d0
mgi166 <skskoari@gmail.com>
2014-07-12 12:25:21 +0900

    * Add new spec #history

--- spec/lib/hipchat_searcher/message_spec.rb
+++ spec/lib/hipchat_searcher/message_spec.rb
@@ -46,4 +46,34 @@ describe HipchatSearcher::Message do
       end
     end
   end
+
+  describe '#history' do
+    before do
+      stub_request_with_headers('https://api.hipchat.com/v2/room/id-12345/history?auth_token=abcd&date=recent&format=JSON&room_id=id-12345&timezone=UTC').to_return(body: response)
+    end
+
+    let(:response) do
+      path = File.join('spec', 'data', 'item-list.json')
+      File.read(path)
+    end
+
+    it 'should return HipchatSearcher::Result instance' do
+      message  = described_class.new('abcd')
+      response = message.history('id-12345')
+      response.should be_instance_of HipchatSearcher::Result
+    end
+
+    it 'when arguments is String should return the room name the same string of arguments' do
+      message  = described_class.new('abcd')
+      response = message.history('id-12345')
+      response.room.should == 'id-12345'
+    end
+
+    it 'when arguments is Hashie::Mash object should return the room name objects given' do
+      room     = Hashie::Mash.new(id: 'id-12345', name: 'room1')
+      message  = described_class.new('abcd')
+      response = message.history(room)
+      response.room.should == 'room1'
+    end
+  end
 end
