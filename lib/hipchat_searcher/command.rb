87a1d1cbc675ab0bebee92304e0dd579f3654f78
mgi166 <skskoari@gmail.com>
2014-09-22 09:44:14 +0900

    * Refactor

--- lib/hipchat_searcher/command.rb
+++ lib/hipchat_searcher/command.rb
@@ -38,25 +38,6 @@ module HipchatSearcher
       Room.new(@options.config.token, @options.room_options).all_room
     end
 
-    def config_valid!
-      unless @options.config.valid?
-        print_no_authorization_token
-        exit 1
-      end
-    end
-
-    def print_no_authorization_token
-      puts <<-EOS
-Authorization token required.
-
-* To create config file, run this command
-`echo {auth_token} > ~/.hps`
-
-* To get new auth_token, visit and sign in.
-https://www.hipchat.com/
-      EOS
-    end
-
     def rooms
       if @options.room?
         room_names = @options.room.split(',')
